03799565d59d1c9936a9c2babcc07815fed1c2cf
Hedgehog <julien.jjd.peynot@gmail.com>
2025-10-03 23:45:04 +0200

    delete test

--- src/test/java/fr/fruityhedgeh0g/MemberControllerIT.java
+++
@@ -1,8 +0,0 @@
-package fr.fruityhedgeh0g;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class MemberControllerIT extends MemberControllerTest {
-    // Execute the same tests but in packaged mode.
-}
